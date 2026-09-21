46011d4776f25809a23dffd2582d9ea3fc9ee234
Leena <leenasri0110@gmail.com>
2026-09-22 01:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,8 @@ if ingredients_list:
             f"Your Smoothie is ordered, {name_on_order}!",
             icon="✅"
         )
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
+st.text(smoothiefroot_response)
