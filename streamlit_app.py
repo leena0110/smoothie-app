125b02caf1b385ccf0863a71de17be6345fae594
Leena <leenasri0110@gmail.com>
2026-09-22 01:10:48 +0530

    Update streamlit_app.py
    Update Snowflake connection for Streamlit Cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,7 +19,8 @@ st.write(
     name_on_order
 )
 
-session = get_active_session()
+conn = st.connection("snowflake")
+session = conn.session()
 
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
