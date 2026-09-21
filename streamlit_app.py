af96c607ee1e48b4b793d3d1fcfd7936ed1dca05
Leena <leenasri0110@gmail.com>
2026-09-22 01:34:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,4 +61,4 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
